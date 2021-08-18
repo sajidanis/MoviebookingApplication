6cd5e84d9d4bfd79742d8141e6c3f1420defa6ee
Md Sajid Anis <>
2021-08-18 19:58:11 +0530

    Added init Service from main application to load some pre data into database

--- src/main/java/com/sajid/moviebookingsystem/MovieBookingSystemApplication.java
+++ src/main/java/com/sajid/moviebookingsystem/MovieBookingSystemApplication.java
@@ -4,10 +4,13 @@ import com.sajid.moviebookingsystem.dao.StatusDao;
 import com.sajid.moviebookingsystem.entities.Movie;
 import com.sajid.moviebookingsystem.entities.Status;
 import com.sajid.moviebookingsystem.exceptions.MovieDetailsNotFoundException;
+import com.sajid.moviebookingsystem.service.InitService;
 import com.sajid.moviebookingsystem.service.MovieService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
 @SpringBootApplication
@@ -16,30 +19,13 @@ public class MovieBookingSystemApplication {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(MovieBookingSystemApplication.class, args);
         System.out.println("Movie Booking Application");
+    }
 
-        MovieService movieService = ctx.getBean(MovieService.class);
-
-        StatusDao statusDao = ctx.getBean(StatusDao.class);
-        Status status = new Status();
-        status.setStatusName("RELEASED");
-        statusDao.save(status);
-
-        Movie movie = new Movie();
-        movie.setMovieName("Inception");
-        movie.setMovieDescription("Best Movie");
-        movie.setCoverPhotoUrl("cover-photo");
-        movie.setDuration(120);
-        movie.setReleaseDate(LocalDateTime.of(2021, 1, 1, 0, 0, 0));
-        movie.setStatus(status);
-        movie.setTrailerUrl("trailer-url");
-        Movie ret = movieService.acceptMovieDetails(movie);
-
-        try {
-            movieService.getMovieDetails(ret.getMovieId());
-        } catch (MovieDetailsNotFoundException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
+    @Bean
+    CommandLineRunner init(InitService initService){
+        return args -> {
+            initService.init();
+        };
     }
 
 }
